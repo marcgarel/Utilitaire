8e6c16ec119dffda351f9f08bc39b57d6d86e06d
MG Macbook Air <marc.garel@mio.osupytheas.fr>
2017-09-06 10:00:16 +0200

    l6 replace command echo by read

--- log_sample.sh
+++ log_sample.sh
@@ -3,7 +3,7 @@
 #MG
 #V1.0
 
-echo "Ceci un programme pour logger les prélèvement. Attention au titre, pensez à changer les titre en fonction des échatillons"
+read -p "Ceci un programme pour logger les prélèvement. Attention au titre, pensez à changer les titre en fonction des échatillons"
 
 read -p 'Entrez un titre: ' titre
 echo "$titre"
@@ -17,8 +17,6 @@ heure=`date "+%H%M%S"`
 
 read -p 'nombre max d échantillon :' maxSample
 read -p 'Entrez le nom de la mission ou de la manip: ' mission
-read -p "-----Prêt pour un nouvelle échantillon------"
-read -p 'Entrez le nom du paramètre échantillonné : ' parm1
 
 var2="$mission $parm1"
 echo "$var2"
@@ -26,6 +24,8 @@ echo "$var2"
 
 for (( i=1; i<=$maxSample; i++))
 do
+read -p "-----Prêt pour un nouvelle échantillon------"
+read -p 'Entrez le nom du paramètre échantillonné : ' parm1
 read -p 'Entrez votre nom : ' Nom
 read -p 'Entrez le volume échantilloné : ' parm2
 read -p 'Ecrire remarque: ' parm3
