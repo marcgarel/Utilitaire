fa3e0f7aab3d8687244d3bec4748da034bdbd93f
MG Macbook Air <marc.garel@mio.osupytheas.fr>
2017-09-06 09:58:40 +0200

    parm1 mis dans la boucle for + adding message for nex sample

--- log_sample.sh
+++ log_sample.sh
@@ -12,12 +12,18 @@ echo "$titre"
 #contenu du script pour remplir tes variables
 Date=`date "+%Y%m%d"`
 heure=`date "+%H%M%S"`
+
+
+
 read -p 'nombre max d échantillon :' maxSample
 read -p 'Entrez le nom de la mission ou de la manip: ' mission
+read -p "-----Prêt pour un nouvelle échantillon------"
 read -p 'Entrez le nom du paramètre échantillonné : ' parm1
 
 var2="$mission $parm1"
 echo "$var2"
+
+
 for (( i=1; i<=$maxSample; i++))
 do
 read -p 'Entrez votre nom : ' Nom
@@ -25,12 +31,9 @@ read -p 'Entrez le volume échantilloné : ' parm2
 read -p 'Ecrire remarque: ' parm3
 var="$Nom: $Date : $heure : $mission : $i : $parm1 : $parm2 : $parm3"
 echo "$var" # etc.... avec ou sans mises en forme c'est a toi de voir
-echo "-----------------------------------"
-echo "Prêt pour un nouvelle échantillon"
-echo "-----------------------------------"
 done
 
-
 ) > "$titre"$(date +%Y%m%d).log
+
 
 exit 0
