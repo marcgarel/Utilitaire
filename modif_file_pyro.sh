ab0495617fff70ede8cd22397d13773f6766aeff
Marc Garel <marc.garel@mio.osupytheas.fr>
2017-09-21 00:12:15 +0200

    modification de la fonction wk

--- modif_file_pyro.sh
+++ modif_file_pyro.sh
@@ -6,5 +6,5 @@ ls *.txt
 for i in *.txt; do
 	echo "$i"
 	j=$(echo "$i" | sed -e "s_\(.*\)\.txt_\1_")
-	cat $i | sed "1, 21d" | awk -F "\t" '{print $1, $2, $3, $5, $6, $9, $10, $17, $18}' > "${j}_last.txt"
+	cat $i | sed "1, 21d" | awk '{print $1, $2, $3, $5, $6, $9, $10, $18, $19}' > "${j}_last.txt"
 done
